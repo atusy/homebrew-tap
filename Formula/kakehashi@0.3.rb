5989081d9b91c43f34560c03dee03be6a29bc33f
atusy <30277794+atusy@users.noreply.github.com>
2026-04-16 23:52:49 +0900

    feat: add kakehashi@0.3 versioned formula (v0.3.0)
    Pin the 0.3 line to v0.3.0 (only release in the 0.3.x series). Same
    `keg_only :versioned_formula` pattern as @0.4 so it can coexist with
    the main formula.

---
+++ Formula/kakehashi@0.3.rb
@@ -0,0 +1,38 @@
+class KakehashiAT03 < Formula
+  desc "Language server bridging the gap between languages, editors, and tooling"
+  homepage "https://github.com/atusy/kakehashi"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/atusy/kakehashi/releases/download/v0.3.0/kakehashi-v0.3.0-aarch64-apple-darwin.tar.gz"
+      sha256 "b6e331fb19012e1f1dc9086f078a422a311df59920f827251db55ec4567c6574"
+    end
+    on_intel do
+      url "https://github.com/atusy/kakehashi/releases/download/v0.3.0/kakehashi-v0.3.0-x86_64-apple-darwin.tar.gz"
+      sha256 "35d9a4cc093a841821bbff60989d50c0602e3d2eeeecaf98136c6816bdb793a3"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/atusy/kakehashi/releases/download/v0.3.0/kakehashi-v0.3.0-aarch64-unknown-linux-gnu.tar.gz"
+      sha256 "96007f27f15e065925035a92e21806ba9d79ba5ce1d774262a7ad40ce9f12982"
+    end
+    on_intel do
+      url "https://github.com/atusy/kakehashi/releases/download/v0.3.0/kakehashi-v0.3.0-x86_64-unknown-linux-gnu.tar.gz"
+      sha256 "a612a5c530e34adc542e2a769e1c25ea6821e60e8021fa2fc2dcb5f68f66d37a"
+    end
+  end
+
+  keg_only :versioned_formula
+
+  def install
+    bin.install "kakehashi"
+  end
+
+  test do
+    assert_match(/\Akakehashi #{Regexp.escape(version.to_s)}\b/,
+                 shell_output("#{bin}/kakehashi --version"))
+  end
+end
